42ba84f03e42aa6ee71e8d9662790e4ab075bfaf
Gerard Genovese <gmoneymaster24@yahoo.com>
2020-02-03 16:27:51 -0500

    Updates

---
+++ precache-manifest.35f2e671f0045fbb1105493740eb01b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02882f6c2bf443ce453972a7f72e879a",
+    "url": "/reactgijoe/index.html"
+  },
+  {
+    "revision": "6ec7828e2df5501942b2",
+    "url": "/reactgijoe/static/css/main.4bc102f9.chunk.css"
+  },
+  {
+    "revision": "6d26cade278791f6853c",
+    "url": "/reactgijoe/static/js/2.b2fe8253.chunk.js"
+  },
+  {
+    "revision": "a290a4b60f8d5284bd7ab92bebea836f",
+    "url": "/reactgijoe/static/js/2.b2fe8253.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ec7828e2df5501942b2",
+    "url": "/reactgijoe/static/js/main.5be9621e.chunk.js"
+  },
+  {
+    "revision": "b3b01e93c80c5201f07f",
+    "url": "/reactgijoe/static/js/runtime-main.ac64596d.js"
+  }
+]);
